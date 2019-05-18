92203c3e196b0284f00bbe0dfbaa444a175219a6
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-18 11:20:14 -0400

    Created a program to find longest word w/out function, used if statement and for loop

---
+++ longestword.js
@@ -0,0 +1,26 @@
+// The quick brown fox jumped over the lazy dog 
+// quasi- code
+// find the longest word
+
+// split the sentence into an array 
+// initialize a longest word variable of type int (0)
+// initialize a longest word variable of type string ("");
+// loop through the array 
+// compare the length of each word
+    // count the letters of the current word (using .length)
+    // store the length in a variable 
+    // 
+var sentence ="The quick brown fox jumped over the lazy dog";
+var array = sentence.split(" ");
+var longestWord = "";
+var longestWordInt = 0;
+
+
+for ( var i = 0; i < array.length; i++) {
+        if ( array[i].length > longestWordInt) {
+            longestWordInt = array[i].length;
+            longestWord = array[i];
+  
+        console.log(longestWord, longestWordInt);
+    }
+}
