b1049891f4ccd8d420e4fdc109d3999ff5b45848
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-21 11:08:33 -0400

    Created arrays, need to sort employees into new array

---
+++ employeesort.js
@@ -0,0 +1,6 @@
+	
+var departments = ["d001", "d002", "d003"];
+
+var employeeDepartments = ["e7001, d003", "e7002, d001", "e7004, d001", "e7003, d002", "e7005, d003", "e7006, d003"];
+
+var employees = [ ];s
