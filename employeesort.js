3f040f37897e778da2a43ce8dde8de3ec5604c55
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-21 12:01:49 -0400

    Used for loop to create multi d array, for employee by department

--- employeesort.js
+++ employeesort.js
@@ -3,4 +3,16 @@ var departments = ["d001", "d002", "d003"];
 
 var employeeDepartments = ["e7001, d003", "e7002, d001", "e7004, d001", "e7003, d002", "e7005, d003", "e7006, d003"];
 
-var employees = [ ];s
+var employees = [ ];
+var tempSplit =[];
+
+for ( var i = 0; i < employeeDepartments.length; i++ ) {
+    employees.push([]);
+}
+for ( var i = 0; i < employeeDepartments.length; i++ ) {
+    tempSplit = employeeDepartments.split(", ");
+    
+    
+}
+console.log(tempSplit);
+console.log(employees);
