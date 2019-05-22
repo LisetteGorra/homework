fe45ca6dd5cd2fca8620df2b6b8aa28c4ebc4a37
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-22 12:40:19 -0400

    Created a program that puts each item from array into sub array, organized by employee by department

--- employeesort.js
+++ employeesort.js
@@ -8,14 +8,18 @@ var tempSplit =[];
 
 for ( var i = 0; i < employeeDepartments.length; i++ ) {
     tempSplit.push(employeeDepartments[i].split(", "));
-    employees.push(tempSplit[i][1]);
+    departments.push(tempSplit[i][1]);
 }
-console.log(tempSplit);
+departments.sort();
+departments = [...new Set(departments)];
+// console.log(departments);
+
 // console.log(employees);
-for ( var i = 0; i < employeeDepartments.length; i++ ) {
+for ( var i = 0; i < departments.length; i++ ) {
     employees.push([]);
 }
 for (var i = 0; i < tempSplit.length; i ++){
-    employees.push(tempSplit[i][0]);
-
+    employees[departments.indexOf(tempSplit[i][1])].push(tempSplit[i][0]);
 }
+
+ console.log(employees);
