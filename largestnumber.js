a91c322aa542b04501f0a30e69b8c4d938506ed5
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-23 13:12:11 -0400

    Created for loop to loop through array

--- largestnumber.js
+++ largestnumber.js
@@ -1,3 +1,8 @@
-numberSets = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
+var numberSets = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
 
 //output the largest number of each set.
+var largestNumber = [];
+
+for ( var i = 0; i < numberSets.length; i ++){
+    console.log(numberSets[i]);
+}
