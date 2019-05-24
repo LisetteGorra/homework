5111d3e33e789c9638ae389fbb6faf8922ad202d
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-24 13:19:24 -0400

    Corrected for loop, added i to nested loop and tested ij in console.log

--- largestnumber.js
+++ largestnumber.js
@@ -4,10 +4,10 @@ var numberSets = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001,
 var largestNumber = [];
 
 for ( var i = 0; i < numberSets.length; i ++){
-    for ( var j = 0; j < numberSets.length; j++){
-      
+    for ( var j = 0; j < numberSets[i].length; j++){
+     console.log(numberSets[i][j]); 
     }
-     console.log(numberSets[i][i]);
+     
 }
 
  
