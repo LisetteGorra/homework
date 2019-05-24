3b32d5a6ffc11aa474d1ae80c568a118bd135bd4
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-24 14:21:25 -0400

    Created if statement to compare numbers, so that the largest number would be stored into empty array varaiable

--- largestnumber.js
+++ largestnumber.js
@@ -4,10 +4,15 @@ var numberSets = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001,
 var largestNumber = [];
 
 for ( var i = 0; i < numberSets.length; i ++){
+    var tempmax = numberSets[i][0];
     for ( var j = 0; j < numberSets[i].length; j++){
-     console.log(numberSets[i][j]); 
+        var currentElement = numberSets[i][j];
+        if (currentElement >= tempmax){
+            tempmax = currentElement;
+        }
+        
     }
-     
+     largestNumber.push(tempmax)
 }
 
- 
+ console.log(largestNumber);
