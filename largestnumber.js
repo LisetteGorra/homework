4406fffe5b28e28647f73fd9bde438bc9422761f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-28 14:25:31 -0400

    Created nested for loops and if statement to select largest numbers from multi-d array with number sets, used .push

--- largestnumber.js
+++ largestnumber.js
@@ -4,15 +4,15 @@ var numberSets = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001,
 var largestNumber = [];
 
 for ( var i = 0; i < numberSets.length; i ++){
-    var tempmax = numberSets[i][0];
+    var tempMax = numberSets[i][0];
     for ( var j = 0; j < numberSets[i].length; j++){
-        var currentElement = numberSets[i][j];
-        if (currentElement >= tempmax){
-            tempmax = currentElement;
+        var loopingThruArr = numberSets[i][j];
+        if (loopingThruArr >= tempMax){
+            tempMax = loopingThruArr;
         }
         
     }
-     largestNumber.push(tempmax)
+     largestNumber.push(tempMax)
 }
 
  console.log(largestNumber);
