e57f728eae141955a854ea21e9019e5ed7ec080e
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-28 10:55:36 -0400

    Reversed a string by using methods .split, .reverse, .join

---
+++ reversestring.js
@@ -0,0 +1,10 @@
+var string ="Life is what you make of it";
+
+var stringSplit = string.split("");
+console.log(stringSplit);
+
+var reverseSplitString = stringSplit.reverse();
+console.log(reverseSplitString);
+
+var joinReversedString = reverseSplitString.join("");
+console.log(joinReversedString);
