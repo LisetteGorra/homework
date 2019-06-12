1616a2a9c8703b3117a6baf1eedd0a42b9201f8a
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-12 17:15:48 -0400

    Creating program that returns the largest number of each subarray

--- largestnumber.js
+++ largestnumber.js
@@ -2,17 +2,19 @@ var numberSets = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001,
 
 //output the largest number of each set.
 var largestNumber = [];
-
+var tempMax = 0;
 for ( var i = 0; i < numberSets.length; i ++){
-    var tempMax = numberSets[i][0];
+    // var tempMax = numberSets[i][0];
     for ( var j = 0; j < numberSets[i].length; j++){
-        var loopingThruArr = numberSets[i][j];
-        if (loopingThruArr >= tempMax){
-            tempMax = loopingThruArr;
+        var maxArrNumber = numberSets[i][j];
+        if (maxArrNumber > largestNumber){
+            // tempMax = maxArrNumber;
+            largestNumber[i] = maxArrNumber;
+          
         }
         
     }
-     largestNumber.push(tempMax)
+   
+    //  largestNumber.push(tempMax)
 }
-
- console.log(largestNumber);
+  console.log(largestNumber);
